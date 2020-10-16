e8426f6076f878c983c6718b07e87c6bbc09ff23
Ujjwal Kumar <katanadeveloper01@gmail.com>
2020-10-16 15:09:20 +0530

    login route and JWT generation

--- controllers/user.controller.ts
+++ controllers/user.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "https://deno.land/x/oak/mod.ts";
 import { MongoClient } from "https://deno.land/x/mongo@v0.12.1/mod.ts";
+import { v4 } from "https://deno.land/std/uuid/mod.ts";
 import { validateJwt } from "https://deno.land/x/djwt/validate.ts";
 import { makeJwt, setExpiration, Jose, Payload } from "https://deno.land/x/djwt/create.ts";
 
@@ -14,23 +15,52 @@ const userModel = db.collection<User>("users");
 
 export default {
     register: async ({request, response}: {request: Request, response: Response})  => {
-        const body: User = await request.body().value
+        const body: User = await request.body().value;
         const email = body.email;
         const exists = await userModel.findOne({email});
         if(exists) { response.body={message: "User Exists"} }
         else{
             const user: User = {
                 name: body.name,
-                email: body.email,
+                id: v4.generate(),
+                email,
                 password: body.password,
             }
-            const newUser = await userModel.insertOne({ name: user.name, email: user.email, password: user.password })
+            await userModel.insertOne({ id: user.id, name: user.name, email, password: user.password })
             response.body = {
                 user
             }
         }
     },
-    login: ({request, response}: {request: Request, response: Response}) => {},
+    login: async ({request, response}: {request: Request, response: Response}) => {
+        const key = "secret";
+        const body: User = await request.body().value;
+        const email = body.email;
+        const password = body.password;
+        const exists = await userModel.findOne({email});
+        if (exists) {
+            if (password !== exists.password) {
+                response.body = { message: "Wrong Password" }
+            } else {
+                const payload = {
+                    email,
+                    exp: setExpiration(2593000),
+                    id: exists.id
+                }
+                const header: Jose = {
+                    alg: "HS256",
+                    typ: "JWT"
+                };
+                const token = await makeJwt({key, header, payload})
+                response.body = {
+                    user: exists,
+                    token
+                }
+            }
+        }else {
+            response.body = { message: "Cant find user" }
+        }
+    },
     getUser: ({request, response}: {request: Request, response: Response}) => {},
     getAll: ({request, response}: {request: Request, response: Response}) => {},
     deleteUser: ({request, response}: {request: Request, response: Response}) => {}
