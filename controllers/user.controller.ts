db933a34c7b1451033ad80d303b873604b5badf8
Ujjwal Kumar <katanadeveloper01@gmail.com>
2020-10-16 14:31:58 +0530

    fixed err handling

--- controllers/user.controller.ts
+++ controllers/user.controller.ts
@@ -1,4 +1,4 @@
-import { Request, Response, ErrorStatus, httpErrors } from "https://deno.land/x/oak/mod.ts";
+import { Request, Response } from "https://deno.land/x/oak/mod.ts";
 import { MongoClient } from "https://deno.land/x/mongo@v0.12.1/mod.ts";
 import { validateJwt } from "https://deno.land/x/djwt/validate.ts";
 import { makeJwt, setExpiration, Jose, Payload } from "https://deno.land/x/djwt/create.ts";
@@ -16,16 +16,18 @@ export default {
     register: async ({request, response}: {request: Request, response: Response})  => {
         const body: User = await request.body().value
         const email = body.email;
-        const exist = await userModel.findOne({email});
-        if(exist) {return }
-        const user: User = {
-            name: body.name,
-            email: body.email,
-            password: body.password,
-        }
-        const newUser = await userModel.insertOne({ name: user.name, email: user.email, password: user.password })
-        response.body = {
-            user: newUser
+        const exists = await userModel.findOne({email});
+        if(exists) { response.body={message: "User Exists"} }
+        else{
+            const user: User = {
+                name: body.name,
+                email: body.email,
+                password: body.password,
+            }
+            const newUser = await userModel.insertOne({ name: user.name, email: user.email, password: user.password })
+            response.body = {
+                user
+            }
         }
     },
     login: ({request, response}: {request: Request, response: Response}) => {},
