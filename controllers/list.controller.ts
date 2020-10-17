36e88837d6664e8df7a27f49a5f9268556e01797
Ujjwal Kumar <katanadeveloper01@gmail.com>
2020-10-17 15:51:53 +0530

    added getItem func with verification

--- controllers/list.controller.ts
+++ controllers/list.controller.ts
@@ -1,5 +1,6 @@
-import { Request, Response } from "https://deno.land/x/oak/mod.ts";
+import { Request, Response, Status } from "https://deno.land/x/oak/mod.ts";
 import { MongoClient } from "https://deno.land/x/mongo@v0.12.1/mod.ts";
+import { validateJwt } from "https://deno.land/x/djwt/validate.ts";
 
 import { ListItem } from "../interfaces/ListItem.ts";
 
@@ -10,15 +11,37 @@ const db = client.database("bucketlist");
 const listModel = db.collection<ListItem>("lists");
 
 export default {
-  getItem: (
-    { request, response }: { request: Request; response: Response },
+  getItem: async (
+    { request, response, params }: {
+      request: Request;
+      response: Response;
+      params: { id: string };
+    },
   ) => {
-
+    const jwt = request.headers.get("authorization")!;
+    const data: any = validateJwt({ jwt, key: "secret", algorithm: "HS256" });
+    if (data.id !== params.id) {
+      response.status = Status.Unauthorized;
+      response.body = {
+        message: "Unauthorized",
+      };
+    } else {
+      const item: ListItem | null = await listModel.findOne({ id: params.id });
+      if (!item) {
+        response.status = Status.NotFound;
+        response.body = {
+          message: "Not Found",
+        };
+      } else {
+        response.body = {
+          item,
+        };
+      }
+    }
   },
   postItem: (
     { request, response }: { request: Request; response: Response },
   ) => {
-
   },
   editItem: (
     { request, response }: { request: Request; response: Response },
